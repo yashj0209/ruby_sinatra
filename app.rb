9773fe7a0a4f0d1c4acf08768bef4108980b3e24
yashj0209 <53044482+yashj0209@users.noreply.github.com>
2021-09-17 10:51:37 +0530

    Update and rename helloWorld.rb to app.rb

--- app.rb
+++ app.rb
@@ -18,18 +18,11 @@ get '/' do
     end
   end
   
-  #There are many useful environment variables available in process.env,
-  #please refer to the following document for detailed description:
-  #http://docs.cloudfoundry.com/docs/using/deploying-apps/environment-variable.html
   
   #VCAP_APPLICATION contains useful information about a deployed application.
   appInfo = @env["VCAP_APPLICATION"]
 
-  #VCAP_SERVICES contains all the credentials of services bound to
-  #this application. For details of its content, please refer to
-  #the document or sample of each service.
   services = @env["VCAP_SERVICES"]
-  #TODO: Get service credentials and communicate with bluemix services.
 
   # render template
   haml :hi
